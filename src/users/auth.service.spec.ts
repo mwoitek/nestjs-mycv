35d2f3f75b8dbb7022fb1c2117ff1bb2fdb6ca01
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-26 17:41:49 -0300

    Test that password gets hashed

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -29,4 +29,13 @@ describe('AuthService', () => {
   it('can create an instance of auth service', () => {
     expect(service).toBeDefined();
   });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('user@example.com', 'testpass123');
+    expect(user.password).not.toEqual('testpass123');
+
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
