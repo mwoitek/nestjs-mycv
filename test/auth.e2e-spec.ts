38c3076a560432fad1de264c369ec95214342827
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-28 19:11:18 -0300

    One more test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,20 @@ describe('Authentication System', () => {
         expect(email).toEqual(test_email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'new_user@example.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'new_pwd' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 });
