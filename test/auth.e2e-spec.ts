617d7e5bfd6858ec2ad08e9e52d074bf78d569df
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-28 13:57:21 -0300

    Add E2E test for authentication system

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const test_email = 'new_user@example.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: test_email, password: 'new_pwd' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+
+        expect(id).toBeDefined();
+        expect(email).toEqual(test_email);
+      });
+  });
+});
