8afad8c963c4d947ba327d80be0aa4ce0882d32d
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-28 13:55:31 -0300

    Use cookie-session globally

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['HVrrhmm65GWdKF2hXhV7OYnDUn6o8n0Bc1v'],
-    })
-  );
   await app.listen(3000);
 }
 bootstrap();
